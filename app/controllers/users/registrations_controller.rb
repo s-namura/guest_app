bfad4fb16a66ea14835d2f1847fae134d14f80e8
s-namura <shin-namura@hi.enjoy.ne.jp>
2021-06-07 22:07:17 +0900

    ゲストユーザーの更新不可設定

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,10 +1,10 @@
 class Users::RegistrationsController <
 Devise::RegistrationsController
-  before_action :ensure_normal_user, only: :destroy
+  before_action :ensure_normal_user, only: %1[update destroy]
 
   def ensure_normal_user
     if resource.email == "guest@example.com"
-      redirect_to root_path, alert: "ゲストユーザーは削除できません。"
+      redirect_to root_path, alert: "ゲストユーザーの更新・削除できません。"
     end
   end
 end
